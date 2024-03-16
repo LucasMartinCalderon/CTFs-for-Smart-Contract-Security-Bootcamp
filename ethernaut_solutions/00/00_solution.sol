e4f5352ecb86051eb28d12ccf9eea733e61bbe03
Lucas Martin Calderon <25382998+LucasMartinCalderon@users.noreply.github.com>
2024-03-16 22:12:24 +0100

    Update 00_solution.sol

--- ethernaut_solutions/00/00_solution.sol
+++ ethernaut_solutions/00/00_solution.sol
@@ -1,1 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Instance {
+
+  string public password;
+  uint8 public infoNum = 42;
+  string public theMethodName = 'The method name is method7123949.';
+  bool private cleared = false;
+
+  // constructor
+  constructor(string memory _password) {
+    password = _password;
+  }
+
+  function info() public pure returns (string memory) {
+    return 'You will find what you need in info1().';
+  }
+
+  function info1() public pure returns (string memory) {
+    return 'Try info2(), but with "hello" as a parameter.';
+  }
+
+  function info2(string memory param) public pure returns (string memory) {
+    if(keccak256(abi.encodePacked(param)) == keccak256(abi.encodePacked('hello'))) {
+      return 'The property infoNum holds the number of the next info method to call.';
+    }
+    return 'Wrong parameter.';
+  }
+
+  function info42() public pure returns (string memory) {
+    return 'theMethodName is the name of the next method.';
+  }
+
+  function method7123949() public pure returns (string memory) {
+    return 'If you know the password, submit it to authenticate().';
+  }
+
+  function authenticate(string memory passkey) public {
+    if(keccak256(abi.encodePacked(passkey)) == keccak256(abi.encodePacked(password))) {
+      cleared = true;
+    }
+  }
+
+  function getCleared() public view returns (bool) {
+    return cleared;
+  }
+}
+
+/* SOLUTION: 
+In the Console of your browser, input the following commands:
+1. $await contract.info()
+2. $await contract.info1()
+3. $await contract.info2("hello")
+4. $await contract.info42()
+5. $await contract.theMethodName
+6. $await contract.method7123949()
+7. $await contract.password
+8. $await contract.authenticate("ethernaut0")
+*/
