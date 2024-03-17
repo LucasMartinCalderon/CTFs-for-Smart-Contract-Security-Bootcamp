8e205d82b0d62c3d7c0b7b255c83185d872fc2fc
Lucas Martin Calderon <25382998+LucasMartinCalderon@users.noreply.github.com>
2024-03-17 13:13:06 +0000

    finished TestBaseSetUp.sol

--- ethernaut_with_foundry/src/Ethernaut.sol
+++ ethernaut_with_foundry/src/Ethernaut.sol
@@ -184,7 +184,7 @@ contract Ethernaut is Ownable {
         address indexed level
     );
 
-    function createLevelInstance(Level _level) public payable {
+    function createLevelInstance(Level _level) public payable returns(address){
         // Ensure level is registered.
         require(registeredLevels[address(_level)], "This level doesn't exists");
 
@@ -202,9 +202,11 @@ contract Ethernaut is Ownable {
 
         // Retrieve created instance via logs.
         emit LevelInstanceCreatedLog(msg.sender, instance, address(_level));
+
+        return instance; 
     }
 
-    function submitLevelInstance(address payable _instance) public {
+    function submitLevelInstance(address payable _instance) public returns(bool) {
         // Get player and level.
         EmittedInstanceData storage data = emittedInstances[_instance];
         require(
@@ -225,12 +227,15 @@ contract Ethernaut is Ownable {
             );
             // Notify success via logs.
             emit LevelCompletedLog(msg.sender, _instance, address(data.level));
+
+            return true; 
         } else {
             statistics.submitFailure(
                 _instance,
                 address(data.level),
                 msg.sender
             );
+            return false; 
         }
     }
 }
--- ethernaut_with_foundry/test/utils/TestBaseSetUp.sol
+++ ethernaut_with_foundry/test/utils/TestBaseSetUp.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.4.0;
 pragma experimental ABIEncoderV2;
 
 import "forge-std/Test.sol"; 
+import {Vm} from "forge-std/Vm.sol";
 import "./TestUtilities.sol";
 import "../../src/Ethernaut.sol";
 
@@ -10,9 +11,70 @@ abstract contract TestBaseSetUp is Test {
     Ethernaut public ethernaut;
     TestUtilities public utils;
 
+    address payable[] internal users; 
+    address payable internal owner; 
+    address payable internal player; 
+    uint256 private userCount; 
+    uint256 private userInitialFunds = 100 ether; 
+    string[] private userLabels;  
+    Level internal levelFactory; 
+    address payable internal levelAddress; 
+
+    constructor() {
+        userCount = 2; 
+        userInitialFunds = 5 ether; 
+        userLabels = new string[](userCount);
+        userLabels[0] = "Owner";
+        userLabels[1] = "Player";
+    }
+
     function setUp() public {
+        require(address(levelFactory) != address(0), "please set up levelFactory");
+
         ethernaut = new Ethernaut();
         utils = new TestUtilities();
+        ethernaut.registerLevel(levelFactory);
+
+        if(userCount > 0) {
+            users = utils.createUsers(userCount, userInitialFunds, userLabels);
+            owner = users[0];
+            player = users[1];
+        }
+    }
+
+    function createLevelInstance(bool _fromPlayer) external payable returns(address) {
+        require(address(levelFactory) != address(0), "please set up levelFactory");
+        require(address(ethernaut) != address(0), "please set up ethernaut");
+        require(address(utils) != address(0), "please set up utils");
+
+        if(_fromPlayer) {
+            vm.prank(player);
+        } 
+
+        return ethernaut.createLevelInstance{value: msg.value}(levelFactory);
+    }
+
+    function runLevel() public {
+        // set up the exploit
+        setUpLevel(); 
+
+        // run the exploit
+        exploitLevel();
+
+        // check if the exploit was successful
+        checkLevel();
+    }
+
+    function setUpLevel() virtual internal;
+    function exploitLevel() virtual internal;
+    function checkLevel() virtual internal {
+        // check if the level was successful
+        vm.startPrank(player);
+        // check if the level was successful
+        bool success = ethernaut.submitLevelInstance(payable(levelAddress));
+        assertTrue(success, "Solution is not solving the level");
+
+        vm.stopPrank();
     }
 }
 
